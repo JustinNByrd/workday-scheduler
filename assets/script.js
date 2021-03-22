2d40bf07e81ba5b16772ae1c5958da2caf4eb975
Justin Byrd <justin_byrd@hotmail.com>
2021-03-22 18:22:41 -0400

    Added code to set background color based on time

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 var now = moment();
+now = now.add(-7, "h");
+var nowHour = now.hour();
 var currentDayEl = $("#currentDay");
 
 // Set currentDay <p> text to the current date
 currentDayEl.text(now.format("dddd, MMMM Do YYYY"));
+
+//Set textarea backgrounds based on past, present or future
+for (i = 9; i < 18; i++) {
+    var textareaEl  = $("#textarea-" + i);
+    if (i < nowHour) {
+        textareaEl.addClass("past");
+    }
+    else if (i == nowHour) {
+        textareaEl.addClass("present");
+    }
+    else
+        textareaEl.addClass("future");
+}
